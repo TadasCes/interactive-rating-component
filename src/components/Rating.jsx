f4f8d62d5157c89f7c411b78679fe08ee2b9dcba
vilkelisv <v.vilkelis97@gmail.com>
2022-03-31 12:12:56 +0300

    fix bugs

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 const Rating = ({ number, selected = false, onSelect }) => {
   return (
     <li className={selected ? 'selected-rating' : ''} onClick={onSelect}>
--- src/components/RatingScale.jsx
+++ src/components/RatingScale.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Rating from './Rating';
 
 const RatingScale = ({ ratingCount, selectRating }) => {
